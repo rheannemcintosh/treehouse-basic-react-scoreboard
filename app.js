d70dcb68dfa01b45345e03781f8b8d39171d9c4a
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-02-01 22:45:52 +0000

    Create players array

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 const Header = () => {
+const players = [
+	{
+		name: "Guil",
+		score: 50,
+		id: 1
+	},
+	{
+		name: "Treasure",
+		score: 85,
+		id: 2
+	},
+	{
+		name: "Ashley",
+		score: 95,
+		id: 3
+	},
+	{
+		name: "James",
+		score: 80,
+		id: 4
+	}
+];
+
 	return (
     	<header>
       		<h1>Scoreboard</h1>
